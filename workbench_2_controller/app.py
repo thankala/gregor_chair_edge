0b30a5da0d2540001466573b0a38648c8747d405
Thanasis Kalatheris <kalatheristhanasis@gmail.com>
2024-06-20 12:57:59 +0300

    Modified workbench 2 controller

--- workbench_2_controller/app.py
+++ workbench_2_controller/app.py
@@ -4,39 +4,27 @@ from flask import Flask, request, jsonify
 
 from gpio.gpio import set_pin_state, get_pin_state, cleanup, LEDPin, LEDState, init_pins
 
-# from jsonschema import validate, ValidationError
-
 app = Flask(__name__)
-
-
-# set_led_schema = {
-#     "type": "object",
-#     "properties": {
-#         "pin": {"type": "string", "enum": ["FREE", "ASSEMBLING", "PENDING"]},
-#         "state": {"type": "string", "enum": ["HIGH", "LOW"]}
-#     },
-#     "required": ["pin", "state"]
-# }
 
 
 @app.route('/pin', methods=['POST'])
 def set_led() -> Any:
     data = request.get_json()
-    # try:
-    #     validate(data, set_led_schema)
-    # except ValidationError as e:
-    #     return jsonify({'error': e.message}), 400
-    pin_name = data.get('pin')
-    state_name = data.get('state')
 
-    try:
-        pin = LEDPin[pin_name.upper()]
-        state = LEDState[state_name.upper()]
-    except KeyError:
-        return jsonify({'error': 'Invalid pin or state name'}), 400
+    if not isinstance(data, dict):
+        return jsonify({'error': 'Invalid data format, must be a dictionary'}), 400
 
-    set_pin_state(pin, state)
-    return jsonify({pin.name: get_pin_state(pin).name}), 200
+    for pin_name, state_name in data.items():
+        try:
+            pin = LEDPin[pin_name.upper()]
+            state = LEDState[state_name.upper()]
+        except KeyError:
+            return jsonify({'error': f'Invalid pin or state name: {pin_name}, {state_name}'}), 400
+
+        set_pin_state(pin, state)
+
+    states = {pin.name: get_pin_state(pin).name for pin in LEDPin}
+    return jsonify(states), 200
 
 
 @app.route('/pins', methods=['GET'])
